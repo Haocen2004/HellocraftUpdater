4cf46988924e41ce43f7abd523b4161972ced7f3
Haocen2004 <haocen.minecraft@qq.com>
2020-02-02 15:48:40 +0800

    fix mod loaded

--- src/main/java/com/superzanti/serversync/RefStrings.java
+++ src/main/java/com/superzanti/serversync/RefStrings.java
@@ -1,7 +1,7 @@
 package com.superzanti.serversync;
 
 public class RefStrings {
-    public static final String MODID = "Pluscraft-sync";
+    public static final String MODID = "pluscraftsync";
     public static final String NAME = "Pluscraft Server Sync mod";
     public static final String VERSION = "2.0.0";
 }
